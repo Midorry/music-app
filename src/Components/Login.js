61a37a68babaaa57106f417941b159e738aede11
Midorry <nguyenvantrantam23052002@gmail.com.com>
2023-05-10 16:11:38 +0700

    Css login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -4,12 +4,16 @@ import { accessUrl } from './spotify';
 
 function Login() {
     return (
-        <div className="login">
+        <div className="login grid place-items-center h-screen bg-[#000]">
             <img
+                className="w-full"
                 src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
                 alt=""
             />
-            <a href={accessUrl} className="text-[#000]">
+            <a
+                href={accessUrl}
+                className="text-[#fff] bg-green p-5 rounded-[99px] font-semibold block"
+            >
                 LOGIN TO SPOTIFY
             </a>
         </div>
